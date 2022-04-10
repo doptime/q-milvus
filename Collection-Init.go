9f93199fd17277a39ed35293e186279aba268c9d
yangkequn <yangkequn@gmail.com>
2022-04-10 08:52:35 +0800

    fix:collectionName

--- Collection-Init.go
+++ Collection-Init.go
@@ -8,13 +8,13 @@ import (
 func (c Collection) Init(milvusAdress string, collectionStruct Entity, partitionName string) *Collection {
 	c.milvusAdress = milvusAdress
 	c.partitionName = partitionName
+	c.collectionName = reflect.Indirect(reflect.ValueOf(collectionStruct)).Type().Name() + "s"
 	if len(partitionName) == 0 {
 		c.partitionName = "_default"
 	}
 
 	//Auto build index, modify IndexField if you want to use other index
 	c.IndexFieldName, c.Index = collectionStruct.Index()
-	c.collectionName = reflect.Indirect(reflect.ValueOf(collectionStruct)).Type().Name() + "s"
 	c.dataStruct = collectionStruct
 	c.BuildOutputFields()
 	c.BuildSchema()
