d6d85a6ff78233a784896af77c261d3f91c272d7
yangkequn <yangkequn@gmail.com>
2022-07-26 19:24:38 +0800

    fixture

--- Collection.go
+++ Collection.go
@@ -80,7 +80,7 @@ func (c *Collection) BuildColumns(structSlice interface{}) (reslt []entity.Colum
 			colume = entity.NewColumnFloat(s.Name, []float32{})
 		} else if s.DataType == entity.FieldTypeInt64 {
 			colume = entity.NewColumnInt64(s.Name, []int64{})
-		} else if s.DataType == entity.FieldTypeString {
+		} else if s.DataType == entity.FieldTypeVarChar || s.DataType == entity.FieldTypeString {
 			colume = entity.NewColumnString(s.Name, []string{})
 		} else if s.DataType == entity.FieldTypeFloatVector {
 			if dim, err = strconv.Atoi(s.TypeParams["dim"]); err != nil {
