c003fb613e82f9f46384470ced65d68334c7fc65
yangkequn <yangkequn@gmail.com>
2022-07-27 10:28:22 +0800

    +auto flush

--- Collection-Insert.go
+++ Collection-Insert.go
@@ -2,16 +2,21 @@ package qmilvus
 
 import (
 	"context"
+	"time"
 
 	"github.com/milvus-io/milvus-sdk-go/v2/client"
 )
+
+//set lastFlushTime to zero
+var lastFlushTime map[string]time.Time = make(map[string]time.Time)
 
 //检查源字段和目标字段的对应关系
 //parameter structSlice may be new data or old data
 
 func (c *Collection) Insert(ctx context.Context, modelSlice interface{}) (err error) {
 	var (
-		_client client.Client
+		_client        client.Client
+		_lastFlushTime time.Time = time.Time{}
 	)
 	//step1: convert []*model.Foo to []*Collection
 	dataSlice := c.ModeSliceToEntitySlice(ctx, modelSlice, c.dataStruct)
@@ -23,7 +28,18 @@ func (c *Collection) Insert(ctx context.Context, modelSlice interface{}) (err er
 	// in a main func, remember to close the client
 	defer _client.Close()
 	columes := c.BuildColumns(dataSlice)
-	_, err = _client.Insert(context.Background(), c.collectionName, c.partitionName, columes...)
+	if _, err = _client.Insert(context.Background(), c.collectionName, c.partitionName, columes...); err != nil {
+		return err
+	}
+	//auto refresh every 4 seconds
+	if _lastFlushTime, _ = lastFlushTime[c.collectionName]; time.Now().Sub(_lastFlushTime) > time.Second {
+		err = _client.Flush(context.Background(), c.collectionName, true)
+		lastFlushTime[c.collectionName] = time.Now()
+		if err != nil {
+			return err
+		}
+	}
+	err = _client.Flush(context.Background(), c.collectionName, true)
 	return err
 	//no need to Flush，milvus auto Flush every second,if Flush too frequently, it will create too many file segment
 }
---
+++ ogg_test.go
@@ -0,0 +1,56 @@
+package qmilvus
+
+import (
+	"context"
+	"testing"
+
+	"github.com/milvus-io/milvus-sdk-go/v2/entity"
+)
+
+//version 1.0 concates all text together,query it's meaning vector as search vector
+// a improved version will recalculate word embedding, and take the everage word embedding as search vector
+
+type OggAction struct {
+	Id     int64     `schema:"in,out" primarykey:"true"`
+	Ogg    string    `schema:"in,out" max_length:"65535"`
+	Vector []float32 `dim:"768" schema:"in"`
+	Score  float32   ``
+}
+
+func (v OggAction) Index() (indexFieldName string, index entity.Index) {
+	index, _ = entity.NewIndexIvfFlat(entity.IP, 768)
+	return "Vector", index
+}
+
+//BuildSearchVector: If your SearchVector is precalculated, Just return the vector
+func (v OggAction) BuildSearchVector(ctx context.Context) (Vector []float32) {
+	return v.Vector
+}
+
+var milvusAdress string = "milvus.vm:19530"
+var OggActionCollection *Collection = NewCollection(milvusAdress, OggAction{}, "")
+
+func TestOgg(t *testing.T) {
+	oggActionList := make([]OggAction, 0)
+	//create random []float32 with 768 dim
+	for i := 200; i < 300; i++ {
+		oggAction := OggAction{
+			Id:     int64(i),
+			Ogg:    "test",
+			Vector: make([]float32, 768),
+			Score:  float32(i),
+		}
+		for j := 0; j < 768; j++ {
+			oggAction.Vector[j] = float32(i)
+		}
+		oggActionList = append(oggActionList, oggAction)
+	}
+	err := OggActionCollection.Insert(context.Background(), oggActionList)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// func main() {
+// 	TestOgg(nil)
+// }
